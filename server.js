d0da7df960d6ca58013ab40b509cc2324b5e1af4
elinmolindr <elinmolinder1@gmail.com>
2026-01-21 13:47:55 +0100

    Change src path for app.js and movies.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-import initApp from "./src/app.js";
-import api from './src/movies.js';
+import initApp from "./SSR/lib/app.js";
+import api from './SSR/lib/movies.js';
 
 const app = initApp(api);
 
-app.listen(5080);
+app.listen(5080, () => {
+  console.log("Servern kör på http://localhost:5080");
+});

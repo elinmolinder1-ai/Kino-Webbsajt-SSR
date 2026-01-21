a14d084d71e4bea5a32c357b213d3b3a8872954e
elinmolindr <elinmolinder1@gmail.com>
2026-01-21 15:33:05 +0100

    Fixed header handlebar

--- SSR/lib/app.js
+++ SSR/lib/app.js
@@ -4,7 +4,18 @@ import express from "express";
 import { engine } from "express-handlebars";
 import path from "path";
 
-
+// Meny-array
+export const MENU = [
+  { label: "Startsida", id: "movies", link: "/" },
+  { label: "Alla filmer", id: "movies", link: "/movies" },
+  { label: "Barnbio", id: "kids", link: "/kids" },
+  { label: "Presentkort", id: "gift", link: "/gift" },
+  { label: "Café & Bistro", id: "cafe", link: "/#cafe-bistro" },
+  { label: "Event", id: "events", link: "/events" },
+  { label: "Kundservice", id: "support", link: "/support" },
+  { label: "Mina sidor", id: "profile", link: "/member-page" },
+  { label: "Företag", id: "business", link: "/business" }
+];
 
 
 export default function initApp(api) {
@@ -15,6 +26,10 @@ export default function initApp(api) {
   app.set("views", path.join(process.cwd(), "SSR", "templates"));
 
 
+
+
+
+
 //Get index.html to be the "home page"
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: path.join(process.cwd(), "public") });
@@ -23,7 +38,7 @@ app.get("/", (req, res) => {
 //Path to all-movies 
 app.get("/movies", async (req, res) => {
   const movies = await api.loadMovies();
-  res.render("all-movies", { movies });
+  res.render("all-movies", { movies, menu: MENU });
 });
   //Path to my static files, in public
   app.use(express.static("public"));

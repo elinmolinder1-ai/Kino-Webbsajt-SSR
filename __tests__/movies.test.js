e3e61cc359c6a22c736bd711c4cf4a1c4d0d2a0d
elinmolindr <elinmolinder1@gmail.com>
2026-01-22 17:21:03 +0100

    Test for movie title

--- __tests__/movies.test.js
+++ __tests__/movies.test.js
@@ -1,35 +1,87 @@
-/* 
+
 import { describe, expect, test } from '@jest/globals';
 import request from 'supertest';
-import initApp from '../SSR/lib/app.js';
-//import fs from 'fs'; // har lagt till denna rad
+import initApp from '../src/app.js';
 
-// Minimal mock API för testet
 const mockApi = {
-  loadMovie: async (id) => ({
-    data: { id, attributes: { title: "The Godfather" } }   // här styr du vilken titel som ska visas
-  }),
-  loadMovies: async () => ({
-    data: [
-      { id: 1, attributes: { title: "The Godfather" } },
-      { id: 2, attributes: { title: "Encanto" } }
-    ]
-  })
+  loadMovie: async (id) => {
+    return {
+      "data": {
+        "id": id,
+        "attributes": {
+          "title": "The Godfather",
+          "imdbId": "tt0068646",
+          "intro": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.\n\n",
+          "image": {
+            "url": "https://m.media-amazon.com/images/M/MV5BNGEwYjgwOGQtYjg5ZS00Njc1LTk2ZGEtM2QwZWQ2NjdhZTE5XkEyXkFqcGc@._V1_.jpg"
+          },
+          "createdAt": "2026-01-15T13:27:00.409Z",
+          "updatedAt": "2026-01-15T13:30:26.154Z",
+          "publishedAt": "2026-01-15T13:27:05.498Z"
+        }
+      },
+    };
+  },
+  loadMovies: async () => {
+    return {
+      "data": [
+        {
+          "id": 12,
+          "attributes": {
+            "title": "The Godfather",
+            "imdbId": "tt0068646",
+            "intro": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.\n\n",
+            "image": {
+              "url": "https://m.media-amazon.com/images/M/MV5BNGEwYjgwOGQtYjg5ZS00Njc1LTk2ZGEtM2QwZWQ2NjdhZTE5XkEyXkFqcGc@._V1_.jpg"
+            },
+            "createdAt": "2026-01-15T13:27:00.409Z",
+            "updatedAt": "2026-01-15T13:30:26.154Z",
+            "publishedAt": "2026-01-15T13:27:05.498Z"
+          }
+        },
+        {
+          "id": 2,
+          "attributes": {
+            "title": "Encanto",
+            "imdbId": "tt2953050",
+            "intro": "A Colombian teenage girl has to face the frustration of being **the only member of her family** without magical powers.\n\n",
+            "image": {
+              "url": "https://m.media-amazon.com/images/M/MV5BOTY1YmU1ZTItMzNjZC00ZGU0LTk0MTEtZDgzN2QwOWVlNjZhXkEyXkFqcGc@._V1_.jpg"
+            },
+            "createdAt": "2023-01-23T06:46:24.765Z",
+            "updatedAt": "2025-01-15T10:41:46.386Z",
+            "publishedAt": "2023-01-23T06:46:29.324Z"
+          }
+        },
+      ],
+      "meta": {
+        "pagination": {
+          "page": 1,
+          "pageSize": 25,
+          "pageCount": 1,
+          "total": 11
+        }
+      }
+    }
+  },
 };
 
-describe("movie", () => {
-  test("shows correct movie title", async () => {
+describe('Movie list page', () => {
+  test('lists movies from API', async () => {
     const app = initApp(mockApi);
 
-    // anropa en specifik film, t.ex. id=1
     const response = await request(app)
-      .get("/movies/1")
+      .get('/')
+      .expect('Content-Type', /html/)
       .expect(200);
 
-    // kontrollera att titeln från mockApi.loadMovie syns i HTML
-    expect(response.text).toContain("The Godfather");
+    expect(response.text).toContain('Encanto');
+    expect(response.text).toContain('The Godfather');
   });
 });
+
+
+
 
 
 
@@ -68,4 +120,4 @@ describe("Integrationstest för filmsidor", () => {
 afterAll(() => {
   server.close();
 });
- */ 
+ */

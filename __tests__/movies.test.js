f281311c91833873d8ca5c9bbc0d47ae7bf62b55
elinmolindr <elinmolinder1@gmail.com>
2026-01-23 10:31:10 +0100

    Comments in the test file

--- __tests__/movies.test.js
+++ __tests__/movies.test.js
@@ -1,29 +1,34 @@
+//import jest-functions, supertest and the function who creates the express app.
 import { describe, expect, test } from '@jest/globals';
 import request from 'supertest';
 import initApp from '../SSR/lib/app.js';
 
-// Mock API som Express kommer använda
+// Mock API that replaces the real API during the test
+// When the app calls api.loadMovie(id), this fake function runs instead
 const mockApi = {
   loadMovie: async (id) => ({
     data: {
       id,
       attributes: {
         title: "The Godfather",
-        intro: "Some intro text",
-        image: { url: "https://example.com/poster.jpg" }
       }
     }
   })
 };
 
+// Test that checks if the movie page shows the correct title
 describe("Movie page", () => {
   test("shows correct movie title", async () => {
+    
+    // Start the Express app using the fake API
     const app = initApp(mockApi);
 
+    // Make a request to the movie page /movies/12 // Supertest acts like a browser and calls the route
     const response = await request(app)
-      .get("/movies/12")   // <-- riktig route
-      .expect(200);
+      .get("/movies/12")  
+      .expect(200); // We expect the page to load successfully
 
+    // Check that the HTML contains the movie title
     expect(response.text).toContain("The Godfather");
   });
 });

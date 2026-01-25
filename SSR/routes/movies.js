4bee9e4c9fa7bf7b94f870e3eb636ca9dc4b3958
elinmolindr <elinmolinder1@gmail.com>
2026-01-25 15:00:37 +0100

    Add footer section imports and pass them to movie routes

--- SSR/routes/movies.js
+++ SSR/routes/movies.js
@@ -1,8 +1,15 @@
-//routes/movies.js = definierar de faktiska routes för filmer
+//routes/movies.js = routes for movies 
 
 import express from "express";
 import {mapMovie, mapMovies} from "../lib/mappers.js";
-import {MENU, FOOTER } from '../lib/constants.js';
+import {
+  MENU,
+  FOOTER_ABOUT,
+  FOOTER_OTHER,
+  FOOTER_CONTACT,
+  FOOTER_SOCIAL,
+  FOOTER_NEWS 
+} from '../lib/constants.js';
 
 export default function movieRoutes(api) {
   const router = express.Router();
@@ -11,15 +18,30 @@ export default function movieRoutes(api) {
   router.get("/", async (req, res) => {
     const payload = await api.loadMovies();
     const movies = mapMovies(payload);
-    res.render("all-movies", { movies, menu: MENU, footer: FOOTER });
+    res.render("all-movies", {      
+      movies,
+      menu: MENU,
+      footerAbout: FOOTER_ABOUT,
+      footerOther: FOOTER_OTHER,
+      footerContact: FOOTER_CONTACT,
+      footerSocial: FOOTER_SOCIAL,
+      footerNews: FOOTER_NEWS });
   });
 
   // Single movie
   router.get("/:movieId", async (req, res) => {
     const payload = await api.loadMovie(req.params.movieId);
     const movie = mapMovie(payload);
-    res.render("movie", { movie, menu: MENU });
+    res.render("movie", {
+      movie,
+      menu: MENU,
+      footerAbout: FOOTER_ABOUT,
+      footerOther: FOOTER_OTHER,
+      footerContact: FOOTER_CONTACT,
+      footerSocial: FOOTER_SOCIAL,
+    footerNews: FOOTER_NEWS});
   });
+
 
   return router;
 }
